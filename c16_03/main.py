16314f6935dfcc902ed79583d204caefef2dea3b
stidsem4 <colin@cgoutte.fr>
2021-03-24 14:02:15 +0100

    Fixups

--- c16_03/main.py
+++ c16_03/main.py
@@ -14,18 +14,18 @@ notes = [note1, note2, note3, note4, note5, note6,note7,note8]
 #a)
 
 def moyenne_eleve1(liste):
-  moyenne = [ item[2]  for item in liste if  item[0]=="eleve1"] 
+  moyenne = [ item[2]  for item in liste if  item[0]=="eleve1"]
   moyenne1 = sum(moyenne)/len(moyenne)
-  return moyenne1    
+  return moyenne1
 
 print(moyenne_eleve1(notes))
 
 #b)
 
 def moyenne_eleve1(liste):
-  moyenne = [ item[2]  for item in liste if  item[0]=="eleve1" and item[1]=="math" ] 
+  moyenne = [ item[2]  for item in liste if  item[0]=="eleve1" and item[1]=="math" ]
   moyenne1 = sum(moyenne)/len(moyenne)
-  return moyenne1    
+  return moyenne1
 
 print(moyenne_eleve1(notes))
 
@@ -33,22 +33,35 @@ print(moyenne_eleve1(notes))
 def moyenne_tuple(notes, eleve, matiere):
   moyenne = [x for x in notes if x[0] == eleve]
   moyenne_matiere = [x for x in moyenne if x[1] == matiere]
-  return sum([n[2] for x in moyenne_matiere ])/len(moyenne_matiere ) 
-
+  return sum([x[2] for x in moyenne_matiere ])/len(moyenne_matiere )
 
 class Note:
+  instances = []
   def __init__(self, eleve, matiere, valeur): #La méthode pour créer un objet
     self.eleve = eleve
     self.matiere = matiere
     self.valeur = valeur
+    self.instances.append(self)
 
+  def __str__(self):
+    return f"{self.__class__.__name__}('{self.eleve}','{self.matiere}','{self.valeur}')"
+
+  @classmethod
+  def vider(cls):
+    cls.instances = []
 
   def afficher(self):
-    print('eleve :', self.eleve, 'matiere :', self.matiere, 'note :', self.valeur)
+      print(self)
 
+  @classmethod
+  def moyenne(cls, eleve=None, matiere=None):
+      print('****', cls.instances)
+      moyenne = [x for x in cls.instances if x.eleve == eleve] if eleve is not None else _notes
+      moyenne_matiere = [x for x in moyenne if x.matiere == matiere] if matiere is not None else moyenne
+      return sum([x.valeur for x in moyenne_matiere])/len(moyenne_matiere)
 
 onote = Note('eleve1', 'maths', 13)
-print(onote.eleve)
+
 print(onote.matiere)
 print(onote.valeur)
 Note.afficher(onote)
@@ -56,32 +69,12 @@ Note.afficher(onote)
 
 #Question5
 
-onotes=[Note(x,y,z) for x,y,z in notes] 
+onotes=[Note(x,y,z) for x,y,z in notes]
 
-  # Question 6
-class Note:
-  def __init__(self, eleve, matiere, valeur): #La méthode pour créer un objet
-    self.eleve = eleve
-    self.matiere = matiere
-    self.valeur = valeur
-  
-  def __str__(self):
-    return  f"{self.__class__.__name__}('{self.eleve}','{self.matiere}','{self.valeur}')" #ce que vous voulez afficher"  
-
-
+Note.moyenne('eleve2', 'math')
 # Question 7
 
 notes_enregistrées = []
-
-class Note:
-  def __init__(self, eleve, matiere, valeur): #La méthode pour créer un objet
-    self.eleve = eleve
-    self.matiere = matiere
-    self.valeur = valeur
-    notes_enregistrées.append(self) 
-
-  def __str__(self):
-    return  f"{self.__class__.__name__}('{self.eleve}','{self.matiere}','{self.valeur}')" #ce que vous voulez afficher"   
 
 
 #Question 8
@@ -94,21 +87,4 @@ def moyenne_Notes(notes, eleve = None, matiere = None):
 
 #Question 9
 
-class Note:
-  instances = []
-  def __init__(self, eleve, matiere, valeur): #La méthode pour créer un objet
-    self.eleve = eleve
-    self.matiere = matiere
-    self.valeur = valeur
-    self.instances.append(self) 
-
-  def __str__(self):
-    return f"{self.__class__.__name__}('{self.eleve}','{self.matiere}','{self.valeur}')"
-
-#Question 10
-
-
-@classmethod 
-def vider(cls):
-  cls.instances = []
 
